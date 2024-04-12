cd7dd3a3288489c8a3ca4b2cd0f5540435f02006
melanie-sr <melanie.saint.roma@gmail.com>
2024-04-12 11:51:17 +0200

    fetch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://whatyearisit-backend-kappa-ten.vercel.app')
+fetch('https://whatyearisit-backend-kappa-ten.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.year;
